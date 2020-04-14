9236a5791b447a7914155a4961f8598b763d2c93
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-14 18:16:41 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,10 @@
     def rescue_princess_peach(status)
-      status == "rescued"
+     if status == "rescued"
       puts "Hooray! Mario has rescued Princess Peach."
+    else
+      nil
     end
 
 ## Comment the below back in to see method scope in action!
     # rescue_princess_peach
     # puts status
-rescue_princess_peach("rescued")
